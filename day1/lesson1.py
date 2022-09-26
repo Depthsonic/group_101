d5f531706d9d7ddbc38e6915a7588b1aa5796bd4
depthsonic <depthsonic@gmail.com>
2022-09-26 13:11:05 +0400

    Lessons

---
+++ day1/lesson1.py
@@ -0,0 +1,12 @@
+name = "rezi"
+surname = "bolkvadze"
+height = 172.5
+
+knows_programming = True
+
+its_ugly = False
+
+print(name + " " + surname)
+
+
+
---
+++ day2/lesson2.py
@@ -0,0 +1,5 @@
+name = "rezi"
+age = 25
+height = 172
+hobby = "photography"
+print("chemi saxelia {} da asakia {} aseve chemi hobbya {}".format(name, age, hobby))
